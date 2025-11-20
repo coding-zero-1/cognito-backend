774f5294513e3e3e17e3bf892ebd698b1fda22e1
coding-zero-I <yashsangwan4700@gmail.com>
2025-11-20 14:12:21 +0530

    added cloudinary config

---
+++ src/config/cloudinaryConfig.ts
@@ -0,0 +1,26 @@
+import { v2 } from "cloudinary";
+import { config } from "dotenv";
+
+config();
+
+const configureCloudinary = async () => {
+  try {
+    if (
+      !process.env.CLOUDINARY_CLOUD_NAME ||
+      !process.env.CLOUDINARY_API_KEY ||
+      !process.env.CLOUDINARY_API_SECRET
+    ) {
+      throw new Error("Cloudinary configuration variables are missing");
+    }
+
+    v2.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+    console.log("Cloudinary configured successfully");
+  } catch (error) {
+    console.error("Error configuring Cloudinary:", error);
+  }
+};
+export default configureCloudinary;
