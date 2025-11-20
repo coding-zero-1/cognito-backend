db5352709c65cd7df354c140a0871fb753a4bfdf
coding-zero-I <yashsangwan4700@gmail.com>
2025-11-20 14:13:24 +0530

    added basic structure

--- src/routes/cardRouter.ts
+++ src/routes/cardRouter.ts
@@ -1,7 +1,20 @@
 // All the card related routes are defined in this file
-import { Router } from "express";
+import { Router, type Request, type Response } from "express";
 
 const cardRouter = Router();
 
+cardRouter.get("/all-cards", (req: Request, res: Response) => {
+  res.send("Get all cards");
+});
+
+cardRouter.post("/create-card", (req: Request, res: Response) => {
+  res.send("Create a new card");
+});
+cardRouter.put("/update-card/:id", (req: Request, res: Response) => {
+  res.send(`Update card with id ${req.params.id}`);
+});
+cardRouter.delete("/delete-card/:id", (req: Request, res: Response) => {
+  res.send(`Delete card with id ${req.params.id}`);
+});
 
 export default cardRouter;
