8e797fe3147edb76df53b72344e3b3a13e4911ce
coding-zero-I <yashsangwan4700@gmail.com>
2025-11-20 14:13:46 +0530

    added cloudinary

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,25 @@ import { config } from 'dotenv';
 import cors from 'cors';
 import connectToDB from './config/dbConfig';
 import userRouter from './routes/userRouter';
+import configureCloudinary from './config/cloudinaryConfig';
+import authMiddleware from './middleware/authMiddleware';
+import cardRouter from './routes/cardRouter';
 
 config();
 const app = express();
 const PORT = process.env.PORT
 app.use(cors({
     methods:["GET","POST","HEAD","DELETE","PUT"],
-    origin:"http://localhost:5173/*"
+    origin:["http://localhost:5173/*"]
 }));
 
 app.use(express.json());
+
 app.use("/api/v1/users",userRouter);
+app.use("/api/v1/card",authMiddleware,cardRouter)
 
 app.listen(PORT,async()=>{
     await connectToDB();
+    await configureCloudinary();
     console.log(`Server started on PORT: ${PORT}`);
 })
